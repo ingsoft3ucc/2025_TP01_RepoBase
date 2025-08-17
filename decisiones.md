310d6959a2b08eae3964d0b229dcc69646844a88
Conrado Verzini <verziniconrado@gmail.com>
2025-08-17 17:32:39 -0300

    Se agrega funcionalidad de conversor de unidades

--- decisiones.md
+++ decisiones.md
@@ -1,6 +1,13 @@
 # **Trabajo Práctico 1**
 # GIT Basico
 
+Primero forkeamos el repositorio base y luego clonamos ese mismo en una carpeta local:
 
+https://github.com/ingsoft3ucc/2025_TP01_RepoBase
+
+Luego configuramos nuestra identidad, con los siguientes comandos:
+
+git config user.name "Conrado Verzini"
+git config user.email "verziniconrado@gmail.com"
 
 
--- src/app.js
+++ src/app.js
@@ -4,3 +4,26 @@ function saludar() {
 }
 
 saludar();
+
+function convertir(valor, desde, hacia) {
+  if (desde === "m" && hacia === "km") {
+    return valor / 1000;
+  }
+  if (desde === "km" && hacia === "m") {
+    return valor * 1000;
+  }
+  if (desde === "C" && hacia === "F") {
+    return (valor * 9) / 5 + 32;
+  }
+  if (desde === "F" && hacia === "C") {
+    return ((valor - 32) * 5) / 9;
+  }
+  return null;
+}
+
+console.log(convertir(1500, "m", "km"))
+console.log(convertir(1, "km", "m"))
+console.log(convertir(100, "C", "F"))
+console.log(convertir(32, "F", "C"))
+
+
